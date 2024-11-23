411dab5265b7fd89b2e7c49bde792c889ac55136
anbuselvan-r <anbuselvan@mahadhi.com>
2024-11-23 14:51:58 +0530

    updated nexconfig

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['swagger-ui-react'],
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  }
   };
 
 module.exports = nextConfig;
